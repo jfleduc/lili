3b53a251a14abc3b9733df2209b50afeba3b95df
Jean-François Leduc <jfleduc@mail.fr>
2018-07-28 09:34:56 +0000

    List links within category

--- html/function/list_links_in_category.php
+++ html/function/list_links_in_category.php
@@ -4,4 +4,30 @@
  */
 function list_links_in_category($dbc, $catid)
   {
+    try
+      {
+        $query = $dbc->prepare('SELECT    `url`,
+                                          `description`
+                                FROM      `link`
+                                WHERE     `category` = :cat_id 
+                                ORDER BY  `url` ASC;');
+        $query->bindValue(':cat_id', $catid, PDO::PARAM_INT);
+
+	// Loop through all rows in the result set
+        if ($query->execute())
+          {
+            while ($row = $query->fetch(PDO::FETCH_ASSOC))
+              {
+                echo '<p><a href="' . $row['url'] . '">' .
+                  $row['url'] . '</a> &ndash; ' .
+                  $row['description'] . '</p>';
+              }
+          }
+      }
+    catch (PDOException $exception)
+      {
+        $logdata = $exception->getCode() . ' ' . $exception->getMessage();
+        error_log ($logdata);
+        die ('Please contact an administrator.');
+      }
   }
